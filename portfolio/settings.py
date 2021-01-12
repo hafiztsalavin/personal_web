2f9cc3e3f7aa8ea21980fc5c2d27f241099ea856
alvin <hafiztsalavin@gmail.com>
2021-01-12 09:46:07 +0700

    deploy 4

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e(7zwftg_z4)okxle6kemn-!i30^6*y%0npb*@^)a$os&rf6wn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hafiztsalavin.herokuapp.com']
 
 
 # Application definition
