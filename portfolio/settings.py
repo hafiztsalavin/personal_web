f7ddaa26c338075830a453de91fc91b09da6a13e
alvin <hafiztsalavin@gmail.com>
2021-01-12 17:07:57 +0700

    add staticfiles

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -131,14 +131,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 # STATIC_ROOT = ''  # for production
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # STATIC_URL = '/static/'
@@ -149,4 +150,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # ]
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
